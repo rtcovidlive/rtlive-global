8836e75fe47f6bd8dba16028d8afc5f8ca724e6c
Laura Helleckes <l.helleckes@fz-juelich.de>
2020-10-21 11:29:46 +0200

    Full detail plot of the model for a region

--- rtlive/plotting.py
+++ rtlive/plotting.py
@@ -134,3 +134,127 @@ def plot_testcount_components(
     axs[0].set_xlim(pandas.to_datetime('2020-03-01'))
     axs[1].set_xlim(pandas.to_datetime('2020-03-01'))
     return fig, axs
+
+
+def plot_density_curves(
+    idata: arviz.InferenceData,
+    vlines: typing.Optional[preprocessing.NamedDates] = None,
+    actual_tests: typing.Optional[pandas.Series] = None,
+    fig=None,
+    axs: numpy.ndarray=None,
+):
+    """ Creates a figure that shows the most important results of the model for a particular region.
+
+    Parameters
+    ----------
+    idata : arviz.InferenceData
+        contains the MCMC trace and observed data
+    vlines : preprocessing.NamedDates
+        dictionary of { datetime.datetime : str } for drawing vertical lines
+    fig : optional, Figure
+        a figure to use (in combination with [axs] argument)
+    axs : optional, array of axes
+        three subplot axes to plot into
+
+    Returns
+    -------
+    fig : matplotlib.Figure
+        the figure
+    (top, center, bottom) : tuple
+        the subplots
+    """
+    # plot symptom onsets and R_t posteriors
+    if not (fig != None and axs is not None):
+        fig, (ax_curves, ax_testcounts, ax_probability, ax_rt) = pyplot.subplots(
+            nrows=4,
+            gridspec_kw={"height_ratios": [3, 1, 1, 2]},
+            dpi=140,
+            figsize=(10, 8),
+            sharex="col",
+        )
+    else:
+        ax_curves, ax_testcounts, ax_probability, ax_rt = axs
+    handles = []
+
+    scale_factor = model.get_scale_factor(idata)
+
+    # top subplot: counts
+    # "infections" and "test_adjusted_positive" are modeled relative.
+    var_label_colors= [
+        ('infections', 'infections', cm.Reds),
+        ('test_adjusted_positive', 'testing delay adjusted', cm.Greens)
+    ]
+
+    for var, label, cmap in var_label_colors:
+        pymc3.gp.util.plot_gp_dist(
+            ax_curves,
+            x=idata.posterior[var].date.values[3:],
+            samples=((idata.posterior[var].stack(sample=('chain', 'draw')) * scale_factor).values.T)[:, 3:],
+            samples_alpha=0,
+            palette=cmap,
+            fill_alpha=.15,
+        )
+        handles.append(ax_curves.fill_between([], [], color=cmap(100), label=label))
+    label="positive tests"
+    ax_curves.set_ylabel("per day", fontsize=15)
+
+    handles.append(
+        ax_curves.bar(
+            idata.constant_data.observed_positive.date.values,
+            idata.constant_data.observed_positive,
+            label=label,
+            alpha=0.5,
+        )
+    )
+    if vlines:
+        plot_vlines(ax_curves, vlines, alignment='bottom')
+    ax_curves.legend(
+        handles=[
+            *handles,
+        ],
+        loc="upper left",
+        frameon=False,
+    )
+
+    ax_testcounts.plot(
+        idata.constant_data.tests.date.values,
+        idata.constant_data.tests.values,
+        color="orange",
+        label="modeled",
+    )
+    if actual_tests is not None:
+        ax_testcounts.bar(
+            actual_tests.index,
+            actual_tests.values,
+            label="actual",
+        )
+    ax_testcounts.legend(frameon=False, loc="upper left")
+    if vlines:
+        plot_vlines(ax_testcounts, { k : "" for k, v in vlines.items() }, alignment="bottom")
+    ax_testcounts.set_ylabel("daily tests")
+
+    # center subplot: probabilities
+    ax_probability.plot(
+        idata.posterior.date, (idata.posterior.r_t > 1).mean(dim=("chain", "draw"))
+    )
+    ax_probability.set_ylim(0, 1)
+    ax_probability.set_ylabel("$p(R_t > 1)$", fontsize=15)
+
+    # bottom subplot: R_t
+    pymc3.gp.util.plot_gp_dist(
+        ax=ax_rt,
+        x=idata.posterior.date.values,
+        samples=idata.posterior.r_t.stack(sample=("chain", "draw")).T.values,
+        samples_alpha=0,
+    )
+    ax_rt.axhline(1, linestyle=":")
+    ax_rt.set_ylabel("$R_t$", fontsize=15)
+    ax_rt.xaxis.set_major_locator(
+        matplotlib.dates.WeekdayLocator(interval=1, byweekday=matplotlib.dates.MO)
+    )
+    ax_rt.xaxis.set_minor_locator(matplotlib.dates.DayLocator())
+    ax_rt.xaxis.set_tick_params(rotation=90)
+    ax_rt.set_ylim(0, 2.5)
+
+    fig.tight_layout()
+    return fig, (ax_curves, ax_testcounts, ax_probability, ax_rt)
